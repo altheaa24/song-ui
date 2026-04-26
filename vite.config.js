a9327906ef7656fb9e41e1f6c2f6d162591cf6f0
altheaa24 <aeluriz.student@ua.edu.ph>
2026-04-26 22:26:05 +0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // ... your existing config (like plugins)
+  preview: {
+    allowedHosts: ['song-ui-2.onrender.com']
+  }
 })
